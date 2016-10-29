f3dad78aca0731e4c70830dc90c9dbf53442f1fe
shigemk2 <i.am.shige@gmail.com>
2016-10-30 08:49:39 +0900

    Add AccountId

--- src/main/scala/com/example/Account.scala
+++ src/main/scala/com/example/Account.scala
@@ -11,3 +11,13 @@ case class Money(value: Double) {
   def -(money: Money): Money = Money(value - money.value)
   def negative(): Money = Money(0 - value)
 }
+
+object AccountId {
+  var currentId = 0
+  def apply(): AccountId = {
+    currentId = currentId + 1
+    AccountId(currentId.toString)
+  }
+}
+
+case class AccountId(id: String)
