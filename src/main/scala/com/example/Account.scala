fa1a2f58101622335c58713b6cdc87e1a3f76168
shigemk2 <i.am.shige@gmail.com>
2016-10-30 08:48:25 +0900

    Add case class Money

--- src/main/scala/com/example/Account.scala
+++ src/main/scala/com/example/Account.scala
@@ -6,4 +6,8 @@ import com.example._
 object AccountDriver extends CompletableApp(17) {
 }
 
-
+case class Money(value: Double) {
+  def +(money: Money): Money = Money(value + money.value)
+  def -(money: Money): Money = Money(value - money.value)
+  def negative(): Money = Money(0 - value)
+}
